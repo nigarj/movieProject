68a9e5ef96505e2b88d23a30590f1c72c1338bd2
nigarj <96123286+nigarj@users.noreply.github.com>
2022-05-30 17:41:54 +0400

    Add files via upload

---
+++ proyekt/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() { 
+        return (
+            <header className="header">
+                <h1 className="header__title">
+                    MovieApp
+                    
+                </h1>
+            </header>
+        );
+    }
+}
+ 
+export default Header;
